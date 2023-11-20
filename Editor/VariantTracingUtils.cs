c6678819be57a4dde69022cb997c3d15654fa024
eldnach <nachd1337@gmail.com>
2023-11-20 13:27:06 +0200

    added keyword descriptions

--- Editor/VariantTracingUtils.cs
+++ Editor/VariantTracingUtils.cs
@@ -19,7 +19,30 @@ public static class VariantTracingUtils
     public static Dictionary<string, string> keywordDescription = new Dictionary<string, string>()
     {
         {"_MAIN_LIGHT_SHADOWS", "Enabled by the active URP Assets (Lighting -> Main Light -> Cast Shadows)" },
-        {"_ADDITIONAL_LIGHT_SHADOWS", "Enabled by the active URP Assets (Lighting -> Additional Lights -> Cast Shadows)" }
+        {"_MAIN_LIGHT_SHADOWS_CASCADE", "Enabled by the active URP Assets (Lighting -> Main Light -> Cast Shadows)" },
+        {"_ADDITIONAL_LIGHTS", "Enabled by the active URP Assets (Lighting -> Additional Lights)" },
+        {"_ADDITIONAL_LIGHTS_VERTEX", "Enabled by the active URP Assets (Lighting -> Additional Lights)" },
+        {"_ADDITIONAL_LIGHT_SHADOWS", "Enabled by the active URP Assets (Lighting -> Additional Lights -> Cast Shadows)" },
+        {"_REFLECTION_PROBE_BLENDING", "Enabled by the active URP Assets (Lighting -> Reflection Probes -> Probe Blending)" },
+        {"_REFLECTION_PROBE_BOX_PROJECTION", "Enabled by the active URP Assets (Lighting -> Reflection Probes -> Box Projection)" },
+        {"_SHADOWS_SOFT", "Enabled by the active URP Assets (Shadows -> Soft Shadows)" },
+        {"_ALPHAPREMULTIPLY_ON", "Enabled by the URP Lit shader (Surface Options -> Blending Mode -> Premultiply)" },
+        {"_CASTING_PUNCTUAL_LIGHT_SHADOW", "Used by URP during shadowm map generation, to differentiate between dir and punctual light shadows" },
+        {"_EMISSION", "Enabled by the URP Lit shader (Surface Inputs -> Emission)" },
+        {"_LIGHT_COOKIES", "Enabled by Cookie Textures in light sources (Light -> Light Cookie -> Cookie)" },
+        {"_LIGHT_LAYERS", "Enabled by the active URP Assets (Lighting -> Show Additional Properties -> Light Layers)" },
+        {"_SCREEN_SPACE_OCCLUSION", "Enabled by the active URP Renderers / Renderer Features (Renderer Features -> SSAO)" },
+        {"_ALPHATEST_ON", "Enabled by the active URP Assets (Rendering -> Terrain Holes)" },
+        {"_RENDER_PASS_ENABLED", "Enabled by the active URP Renderers (RenderPass -> Native RenderPass)" },
+        {"_DBUFFER_MRT1", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_DBUFFER_MRT2", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_DBUFFER_MRT3", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_DECAL_NORMAL_BLEND_LOW", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_DECAL_NORMAL_BLEND_MEDIUM", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_DECAL_NORMAL_BLEND_HIGH", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_DECAL_LAYERS", "Enabled by the active URP Renderers / Renderer Features (Renderer Feature -> Decals)" },
+        {"_USE_FAST_SRGB_LINEAR_CONVERSION", "Enabled by the active URP Assets (Post Processing -> Fast sRBG/Linear Conversions)" },
+        {"_GBUFFER_NORMALS_OCT", "Enabled by the active URP Renderers (Rendering -> Rendering Path -> Deferred -> Accurate G-buffer normals)" }
     };
 
     public static VariantTrace GetVariantTraceFromFile(string filePath)
